ae4787d17c218cab163d80e214b403304162dc62
Durant Schoon <durant.schoon@freenome.com>
2020-05-03 20:56:27 -0700

    update userconfig with my avy settings

--- init.el
+++ init.el
@@ -78,7 +78,8 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(buffer-move
+   dotspacemacs-additional-packages '(avy
+                                      buffer-move
                                       multiple-cursors
                                       use-package-chords)
    ;; A list of packages that cannot be updated.
