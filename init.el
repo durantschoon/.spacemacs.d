b21fdb90ec4045c1bfa144c4a0d4015fef7771d5
Durant Schoon <durant.schoon@freenome.com>
2020-05-12 17:44:40 -0700

    save will run make lint and test for py files

--- init.el
+++ init.el
@@ -387,5 +387,5 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(comint-highlight-prompt ((t (:inherit minibuffer-prompt :foreground "gray80")))))
 )
