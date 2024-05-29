db12ec7390553967bc4bbb86575b357fabeeb49e
Durant Schoon <durant.schoon@gmail.com>
2024-05-29 16:26:38 -0400

    whitespace only

--- init.el
+++ init.el
@@ -673,7 +673,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
-  
+
   (keychain-refresh-environment)
 
   (org-babel-do-load-languages
