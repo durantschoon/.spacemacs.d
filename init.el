4150217b9916c79602529685e46e705e90fbcc0f
Durant Schoon <durant.schoon@gmail.com>
2024-08-31 19:07:16 -0400

    adjust prompt colors to light mode in shell

--- init.el
+++ init.el
@@ -677,6 +677,26 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
 
+  (defun my-customize-shell-prompt-for-light-theme ()
+    "Customize shell prompt color for light themes like modus-operandi-deuteranopia."
+    (if (member 'modus-operandi-deuteranopia custom-enabled-themes)
+        (custom-set-faces
+         ;; Adjust the colors as desired
+         '(defface ansi-color-black
+            '((t :foreground "cyan" :background "black"))
+            "Face used to render black color code."
+            :group 'ansi-colors
+            :version "28.1"))
+      (custom-set-faces
+       ;; Default or dark theme colors
+       '(defface ansi-color-black
+          '((t :foreground "black" :background "black"))
+          "Face used to render black color code."
+          :group 'ansi-colors
+          :version "28.1"))))
+
+  ;; Hook to run after changing themes
+  (add-hook 'after-load-theme-hook 'my-customize-shell-prompt-for-light-theme)
 
   ;; use ~/.authinfo.gpg with emacs
   (require 'epa-file)
