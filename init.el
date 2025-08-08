879845bfb70a5fd8439528d8f418bbd96f91b1ce
Durant Schoon <durant.schoon@gmail.com>
2025-08-08 15:33:33 -0400

    add adjust-date-on-line to increment dates, works

--- init.el
+++ init.el
@@ -1277,10 +1277,118 @@ SCHEDULED: %^t
   ;; ** 🛠️ Utility Functions **
   ;; ======================================================================
 
-
-
   (defun insert-current-date () (interactive)
          (insert (shell-command-to-string "echo -n $(date +%Y-%m-%d)")))
+
+  (defun find-date-on-line ()
+    "Find the first recognizable date on the current line.
+Returns (START END DATE-OBJECT HAS-DAY-OF-WEEK) or nil if no date found.
+Supports formats: 'Day Month D, YYYY' and 'Day M/D/YY'."
+    (let ((case-fold-search t)
+          (day-month-date-regex
+           "\\b\\([A-Z][a-z]+ [A-Z][a-z]+ [0-9]\\{1,2\\}, [0-9]\\{4\\}\\)\\b")
+          (day-slash-date-regex
+           "\\b\\([A-Z][a-z]+ [0-9]\\{1,2\\}/[0-9]\\{1,2\\}/[0-9]\\{2\\}\\)\\b")
+          (month-date-regex
+           "\\b\\([A-Z][a-z]+ [0-9]\\{1,2\\}, [0-9]\\{4\\}\\)\\b")
+          (slash-date-regex
+           "\\b\\([0-9]\\{1,2\\}/[0-9]\\{1,2\\}/[0-9]\\{2\\}\\)\\b"))
+      (beginning-of-line)
+      (cond
+       ;; Day Month D, YYYY (e.g., "Friday August 08, 2025")
+       ((re-search-forward day-month-date-regex (line-end-position) t)
+        (let* ((date-str (match-string 1))
+               (start-pos (match-beginning 1))
+               (end-pos (match-end 1))
+               (parts (split-string date-str " "))
+               (day-name (car parts))
+               (date-without-day (string-join (cdr parts) " "))
+               (parsed (parse-time-string date-without-day)))
+          (message "DEBUG: Found day-month date: '%s' -> '%s' -> %s" date-str date-without-day parsed)
+          (if parsed
+              (list start-pos end-pos parsed t)
+            nil)))
+       ;; Day M/D/YY (e.g., "Friday 8/8/25")
+       ((re-search-forward day-slash-date-regex (line-end-position) t)
+        (let* ((date-str (match-string 1))
+               (start-pos (match-beginning 1))
+               (end-pos (match-end 1)))
+          (message "DEBUG: Match found - start=%s, end=%s, date-str='%s'" start-pos end-pos date-str)
+          (let* ((parts (split-string date-str " "))
+                 (day-name (car parts))
+                 (date-part (cadr parts))
+                 (date-parts (split-string date-part "/"))
+                 (month (string-to-number (car date-parts)))
+                 (day (string-to-number (cadr date-parts)))
+                 (year (string-to-number (caddr date-parts)))
+                 (century (if (< year 100) (+ 2000 year) year))
+                 (parsed (encode-time 0 0 0 day month century)))
+            (message "DEBUG: Found day-slash date: '%s' -> parsed as %s" date-str parsed)
+            (list start-pos end-pos parsed t))))
+       ;; Month D, YYYY (without day of week)
+       ((re-search-forward month-date-regex (line-end-position) t)
+        (let* ((date-str (match-string 1))
+               (start-pos (match-beginning 1))
+               (end-pos (match-end 1))
+               (parsed (parse-time-string date-str)))
+          (message "DEBUG: Found month date: '%s' -> %s" date-str parsed)
+          (if parsed
+              (list start-pos end-pos parsed nil)
+            nil)))
+       ;; M/D/YY (without day of week)
+       ((re-search-forward slash-date-regex (line-end-position) t)
+        (let* ((date-str (match-string 1))
+               (start-pos (match-beginning 1))
+               (end-pos (match-end 1))
+               (date-parts (split-string date-str "/"))
+               (month (string-to-number (car date-parts)))
+               (day (string-to-number (cadr date-parts)))
+               (year (string-to-number (caddr date-parts)))
+               (century (if (< year 100) (+ 2000 year) year))
+               (parsed (encode-time 0 0 0 day month century)))
+          (message "DEBUG: Found slash date: '%s' -> parsed as %s" date-str parsed)
+          (list start-pos end-pos parsed nil))
+        (t (message "DEBUG: No date found on line") nil)))))
+
+  (defun adjust-date-on-line (days)
+    "Adjust the first recognizable date on the current line by DAYS.
+Supports formats: 'Day Month D, YYYY' and 'Day M/D/YY'."
+    (interactive "nDays to adjust by: ")
+    (let ((date-info (find-date-on-line)))
+      (message "DEBUG: date-info = %s" date-info)
+      (if date-info
+          (let* ((start (nth 0 date-info))
+                 (end (nth 1 date-info))
+                 (date-obj (nth 2 date-info))
+                 (has-day-of-week (nth 3 date-info)))
+            (message "DEBUG: start=%s, end=%s, date-obj=%s, has-day=%s" start end date-obj has-day-of-week)
+            (if (and date-obj start end)
+                (let* ((adjusted-time (if (and (listp date-obj) (>= (length date-obj) 6))
+                                          ;; parse-time-string format: (SEC MIN HOUR DAY MONTH YEAR DOW DST TZ)
+                                          (encode-time 0 0 0
+                                                       (+ (nth 3 date-obj) days)
+                                                       (nth 4 date-obj)
+                                                       (nth 5 date-obj))
+                                        ;; encode-time format: need to decode and re-encode
+                                        (let ((decoded (decode-time date-obj)))
+                                          (encode-time 0 0 0
+                                                       (+ (nth 3 decoded) days)
+                                                       (nth 4 decoded)
+                                                       (nth 5 decoded)))))
+                       (new-date (if has-day-of-week
+                                     (format-time-string "%A %B %-d, %Y" adjusted-time)
+                                   (format-time-string "%B %-d, %Y" adjusted-time))))
+                  (message "DEBUG: adjusted-time=%s, new-date=%s" adjusted-time new-date)
+                  (save-excursion
+                    (goto-char start)
+                    (delete-region start end)
+                    (insert new-date))
+                  (message "Adjusted date: %s" new-date))
+              (message "Invalid date object or position: date-obj=%s, start=%s, end=%s" date-obj start end)))
+        (message "No date found on current line"))))
+
+  (global-set-key (kbd "s-<up>") (lambda () (interactive) (adjust-date-on-line 1)))
+  (global-set-key (kbd "s-<down>") (lambda () (interactive) (adjust-date-on-line -1)))
 
   (defun my-zap-to-char ()
     "Kill up to the ARG'th occurence of CHAR, and leave CHAR. If
