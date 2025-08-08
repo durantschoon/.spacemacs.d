a016954781ed2912d3ed133ed1e7db33d50dcd8e
Durant Schoon <durant.schoon@gmail.com>
2025-08-08 16:24:47 -0400

    adjust-date-format replaces with same format now

--- init.el
+++ init.el
@@ -1362,8 +1362,10 @@ Supports formats: 'Day Month D, YYYY' and 'Day M/D/YY'."
           (let* ((start (nth 0 date-info))
                  (end (nth 1 date-info))
                  (date-obj (nth 2 date-info))
-                 (has-day-of-week (nth 3 date-info)))
+                 (has-day-of-week (nth 3 date-info))
+                 (original-date (buffer-substring start end)))
             (message "DEBUG: start=%s, end=%s, date-obj=%s, has-day=%s" start end date-obj has-day-of-week)
+            (message "DEBUG: original-date='%s'" original-date)
             (if (and date-obj start end)
                 (let* ((adjusted-time (if (and (listp date-obj) (>= (length date-obj) 6))
                                           ;; parse-time-string format: (SEC MIN HOUR DAY MONTH YEAR DOW DST TZ)
@@ -1377,9 +1379,7 @@ Supports formats: 'Day Month D, YYYY' and 'Day M/D/YY'."
                                                        (+ (nth 3 decoded) days)
                                                        (nth 4 decoded)
                                                        (nth 5 decoded)))))
-                       (new-date (if has-day-of-week
-                                     (format-time-string "%A %B %-d, %Y" adjusted-time)
-                                   (format-time-string "%B %-d, %Y" adjusted-time))))
+                       (new-date (adjust-date-format original-date adjusted-time has-day-of-week)))
                   (message "DEBUG: adjusted-time=%s, new-date=%s" adjusted-time new-date)
                   (save-excursion
                     (goto-char start)
@@ -1388,6 +1388,39 @@ Supports formats: 'Day Month D, YYYY' and 'Day M/D/YY'."
                   (message "Adjusted date: %s" new-date))
               (message "Invalid date object or position: date-obj=%s, start=%s, end=%s" date-obj start end)))
         (message "No date found on current line"))))
+
+  (defun adjust-date-format (original-date adjusted-time has-day-of-week)
+    "Format the adjusted time to match the original date format.
+ORIGINAL-DATE is the original date string, ADJUSTED-TIME is the adjusted time object,
+HAS-DAY-OF-WEEK indicates if the original had a day of week."
+    (let* ((decoded (decode-time adjusted-time))
+           (day (nth 3 decoded))
+           (month (nth 4 decoded))
+           (year (nth 5 decoded))
+           (weekday (nth 6 decoded)))
+      (cond
+       ;; Check for slash format (8/8/25 or Friday 8/8/25)
+       ((string-match "\\b\\([A-Z][a-z]+ \\)?\\([0-9]\\{1,2\\}/[0-9]\\{1,2\\}/[0-9]\\{2\\}\\)\\b" original-date)
+        (let ((day-name (if has-day-of-week
+                            (format-time-string "%A " adjusted-time)
+                          "")))
+          (concat day-name (format-time-string "%-m/%-d/%y" adjusted-time))))
+       ;; Check for month format with leading zero (August 08, 2025 or Friday August 08, 2025)
+       ((string-match "\\b\\([A-Z][a-z]+ \\)?\\([A-Z][a-z]+ [0-9]\\{2\\}, [0-9]\\{4\\}\\)\\b" original-date)
+        (let ((day-name (if has-day-of-week
+                            (format-time-string "%A " adjusted-time)
+                          "")))
+          (concat day-name (format-time-string "%B %02d, %Y" adjusted-time))))
+       ;; Check for month format without leading zero (August 8, 2025 or Friday August 8, 2025)
+       ((string-match "\\b\\([A-Z][a-z]+ \\)?\\([A-Z][a-z]+ [0-9]\\{1,2\\}, [0-9]\\{4\\}\\)\\b" original-date)
+        (let ((day-name (if has-day-of-week
+                            (format-time-string "%A " adjusted-time)
+                          "")))
+          (concat day-name (format-time-string "%B %-d, %Y" adjusted-time))))
+       ;; Default fallback
+       (t (if has-day-of-week
+              (format-time-string "%A %B %-d, %Y" adjusted-time)
+            (format-time-string "%B %-d, %Y" adjusted-time))))))
 
   (global-set-key (kbd "s-<up>") (lambda () (interactive) (adjust-date-on-line 1)))
   (global-set-key (kbd "s-<down>") (lambda () (interactive) (adjust-date-on-line -1)))
