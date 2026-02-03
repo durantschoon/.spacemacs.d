62e7fe2f54bb46f9835c7525119005df1f7af35b
Durant Schoon <durant.schoon@gmail.com>
2026-02-03 06:48:21 -0500

    scale down spacemacs icon on dashboard

--- init.el
+++ init.el
@@ -315,7 +315,7 @@ It should only modify the values of Spacemacs settings."
    ;; contents, to a maximum of the full image height and a minimum of 3 line
    ;; heights. If set to a number (int or float) it is used as a constant
    ;; scaling factor for the default logo size.
-   dotspacemacs-startup-banner-scale 'auto
+   dotspacemacs-startup-banner-scale 0.2
 
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
