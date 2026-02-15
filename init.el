e7f5a35e1824628e079a5f61ca3fc5a06580d4e6
Durant Schoon <durant.schoon@gmail.com>
2026-02-15 00:11:46 -0500

    fix: use correct font name 'Fira Code' to resolve warning

--- init.el
+++ init.el
@@ -393,7 +393,7 @@ It should only modify the values of Spacemacs settings."
    ;; fixed-pitch faces. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("FiraCode Nerd Font Mono"
+   dotspacemacs-default-font '("Fira Code"
                                :size 12
                                :weight normal
                                :width normal
